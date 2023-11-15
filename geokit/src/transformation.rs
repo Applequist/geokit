3d04ad798297527a961d8a0a863683f8d1125e71
Brieuc Desoutter <brieuc.desoutter@gmail.com>
2023-11-15 10:07:39 +0300

    Add Identity transformation.

--- geokit/src/transformation.rs
+++ geokit/src/transformation.rs
@@ -1,6 +1,7 @@
 use crate::coord::Coord3D;
 use std::fmt::Debug;
 
+#[derive(Debug)]
 pub enum TransformationError {}
 
 pub trait Transformation: Debug {
@@ -9,7 +10,7 @@ pub trait Transformation: Debug {
 }
 
 pub trait InversibleTransformation: Transformation {
-    fn inverse(&self) -> Box<dyn Transformation>;
+    fn inverse(&self) -> Box<dyn InversibleTransformation>;
     fn boxed_clone(&self) -> Box<dyn InversibleTransformation>;
 }
 
@@ -22,5 +23,47 @@ impl Clone for Box<dyn Transformation> {
 impl Clone for Box<dyn InversibleTransformation> {
     fn clone(&self) -> Box<dyn InversibleTransformation> {
         InversibleTransformation::boxed_clone(&**self)
+    }
+}
+
+#[derive(Debug)]
+pub struct Identity;
+
+impl Transformation for Identity {
+    fn apply(&self, input: &Coord3D) -> Result<Coord3D, TransformationError> {
+        Ok(*input)
+    }
+
+    fn boxed_clone(&self) -> Box<dyn Transformation> {
+        Box::new(Identity)
+    }
+}
+
+impl InversibleTransformation for Identity {
+    fn inverse(&self) -> Box<dyn InversibleTransformation> {
+        Box::new(Identity)
+    }
+
+    fn boxed_clone(&self) -> Box<dyn InversibleTransformation> {
+        Box::new(Identity)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn box_clone() {
+        let boxed_transfo: Box<dyn Transformation> = Box::new(Identity);
+        let _cloned_as_transfo: Box<dyn Transformation> = boxed_transfo.clone();
+        let p = _cloned_as_transfo.apply(&(1., 2., 3.)).unwrap();
+        assert_eq!(p, (1., 2., 3.));
+
+        let boxed_inv_transfo: Box<dyn InversibleTransformation> = Box::new(Identity);
+        let _cloned_as_inv_transfo: Box<dyn InversibleTransformation> = boxed_inv_transfo.clone();
+        let inv_t = _cloned_as_inv_transfo.inverse();
+        assert_eq!(inv_t.apply(&p).unwrap(), (1., 2., 3.));
     }
 }
