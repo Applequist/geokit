6e6b7a8d6d4f99a92a3449fcba3b6ecfab45566e
Brieuc Desoutter <brieuc.desoutter@gmail.com>
2023-11-15 22:59:28 +0300

    Improve test code.

--- geokit/tests/llh_to_xyz.rs
+++ geokit/tests/llh_to_xyz.rs
@@ -1,4 +1,5 @@
 use geokit::geodesy::Ellipsoid;
+use itertools::Itertools;
 use regex::Regex;
 use std::default::Default;
 
@@ -10,24 +11,20 @@ fn llh_to_xyz() {
     let ll_grid = include_str!("data/ll_grid.txt");
     let llh_it = ll_grid
         .lines()
-        .filter(|l| !l.is_empty() && !l.starts_with("#"))
-        .map(|l| {
+        .filter(|l| !l.is_empty() && !l.starts_with('#'))
+        .flat_map(|l| {
             re.split(l.trim())
-                .map(|s| s.parse::<f64>().unwrap())
-                .collect::<Vec<f64>>()
+                .map(|s| s.parse::<f64>().unwrap().to_radians())
+                .chain(std::iter::once(0.))
         })
-        .map(|v| (v[0].to_radians(), v[1].to_radians(), 0.0));
+        .tuples::<(_, _, _)>();
 
     let xyz_grid = include_str!("data/xyz_grid.txt");
     let xyz_it = xyz_grid
         .lines()
-        .filter(|l| !l.is_empty() && !l.starts_with("#"))
-        .map(|l| {
-            re.split(l.trim())
-                .map(|s| s.parse::<f64>().unwrap())
-                .collect::<Vec<f64>>()
-        })
-        .map(|v| (v[0], v[1], v[2]));
+        .filter(|l| !l.is_empty() && !l.starts_with('#'))
+        .flat_map(|l| re.split(l.trim()).map(|s| s.parse::<f64>().unwrap()))
+        .tuples::<(_, _, _)>();
 
     for (llh, xyz) in llh_it.zip(xyz_it) {
         let computed_xyz = ellipsoid.llh_to_xyz(&llh);
