8305e3ed51bf54a151ac143ade444b68ff567787
Brieuc Desoutter <brieuc.desoutter@gmail.com>
2023-11-15 14:01:08 +0300

    Add goedetic to geocentric conversions. Simplified Ellipsoid.

--- geokit/src/geodesy/ellipsoid.rs
+++ geokit/src/geodesy/ellipsoid.rs
@@ -1,14 +1,14 @@
-/// The parameters defining an [Ellipsoid].
-#[derive(Debug, Copy, Clone, PartialEq)]
-enum EllipsoidParams {
-    AB { a: f64, b: f64 },
-    AInvf { a: f64, invf: f64 },
-}
+use crate::coord::Coord3D;
 
 /// An `Ellipsoid` is a mathematical surface used as a model of the Earth surface.
 #[derive(Debug, Copy, Clone, PartialEq)]
 pub struct Ellipsoid {
-    params: EllipsoidParams,
+    /// The semi major axis length **in meters**.
+    pub a: f64,
+    /// The semi minor axis length **in meters**.
+    pub b: f64,
+    /// The inverse flattening: `a / (a - b)``. INFINITY if a == b.
+    pub invf: f64,
 }
 
 impl Ellipsoid {
@@ -21,7 +21,9 @@ impl Ellipsoid {
         assert!(b > 0., "Expected b > 0. Got {}", b);
         assert!(a >= b, "Expected a >= b. Got a = {}, b = {}", a, b);
         Self {
-            params: EllipsoidParams::AB { a, b },
+            a,
+            b,
+            invf: if a > b { a / (a - b) } else { f64::INFINITY },
         }
     }
 
@@ -34,8 +36,90 @@ impl Ellipsoid {
         assert!(a > 0., "Expected a > 0. Got {}", a);
         assert!(invf > 1., "Expected invf > 1. Got {}", invf);
         Self {
-            params: EllipsoidParams::AInvf { a, invf },
+            a,
+            b: a * (1. - 1. / invf),
+            invf,
         }
+    }
+
+    /// Returns whether the ellipsoid is actually a sphere or not.
+    pub fn is_spherical(&self) -> bool {
+        self.invf.is_infinite()
+    }
+
+    /// Returns the semi major axis squared.
+    #[inline]
+    pub fn a_sq(&self) -> f64 {
+        self.a * self.a
+    }
+
+    /// Returns the semi minor axis squared.
+    #[inline]
+    pub fn b_sq(&self) -> f64 {
+        self.b * self.b
+    }
+
+    /// Returns the first eccentricity squared: `(a*a -b*b) / a*a`
+    #[inline]
+    pub fn e_sq(&self) -> f64 {
+        (self.a_sq() - self.b_sq()) / self.a_sq()
+    }
+
+    /// Returns the radius of curvature **in meters** in the east-west direction
+    /// at the given latitude **in radians**.
+    pub fn prime_vertical_radius(&self, lat: f64) -> f64 {
+        self.a / (1.0 - self.e_sq() * lat.sin().powi(2)).sqrt()
+    }
+
+    /// Returns the radius of curvature **in meters** in the north-south direction
+    /// at the given latitude **in radians**.
+    pub fn prime_meridional_radius(&self, lat: f64) -> f64 {
+        self.a * (1.0 - self.e_sq()) / (1.0 - self.e_sq() * lat.sin().powi(2)).powf(1.5)
+    }
+
+    /// Convert **normalized geodetic coordinates** (lon in rad, lat in rad, height in meters)
+    /// into **normalized geocentric coordinates** (x, y, z) all in meters.
+    pub fn llh_to_xyz(&self, &(lon, lat, h): &Coord3D) -> Coord3D {
+        let v = self.prime_vertical_radius(lat);
+        let (sin_lon, cos_lon) = lon.sin_cos();
+        let (sin_lat, cos_lat) = lat.sin_cos();
+        (
+            (v + h) * cos_lat * cos_lon,
+            (v + h) * cos_lat * sin_lon,
+            (v * (1.0 - self.e_sq()) + h) * sin_lat,
+        )
+    }
+
+    /// Convert **normalized geocentric coordinates** (x, y, z) in meters
+    /// into **normalized geodetic coordinates** (lon in rad, lat in rad, height in meters)
+    /// using Heiskanen and Moritz iterative method.
+    pub fn xyz_to_llh(&self, &(x, y, z): &Coord3D) -> Coord3D {
+        let a2 = self.a_sq();
+        let b2 = self.b_sq();
+        let e2 = self.e_sq();
+
+        let lon = y.atan2(x);
+
+        let p = x.hypot(y);
+        let mut lat = z.atan2(p * (1.0 - e2));
+        let (sin_lat, cos_lat) = lat.sin_cos();
+        let n = a2 / (a2 * cos_lat * cos_lat + b2 * sin_lat * sin_lat).sqrt();
+        let mut h = p / cos_lat - n;
+        loop {
+            let next_lat = z.atan2(p * (1.0 - e2 * n / (n + h)));
+            let (sin_nlat, cos_nlat) = next_lat.sin_cos();
+            let next_n = a2 / ((a2 * cos_nlat * cos_nlat) + b2 * sin_nlat * sin_nlat).sqrt();
+            let next_h = p / cos_nlat - next_n;
+            let delta_lat = (lat - next_lat).abs();
+            let delta_h = (h - next_h).abs();
+            lat = next_lat;
+            h = next_h;
+            if delta_lat < 0.5e-5 && delta_h < 0.5e-3 {
+                break;
+            }
+        }
+
+        (lon, lat, h)
     }
 }
 
@@ -92,7 +176,7 @@ mod tests {
         let e = Ellipsoid::from_ab(1., 0.9);
         let cpy = e;
         assert_eq!(e, cpy);
-        let _s = e.params;
+        let _a = e.a;
     }
 
     #[test]
@@ -114,12 +198,7 @@ mod tests {
     #[test]
     fn default() {
         let wgs84 = Ellipsoid::default();
-        assert_eq!(
-            wgs84.params,
-            EllipsoidParams::AInvf {
-                a: 6_378_137.0,
-                invf: 298.257_223_563
-            }
-        );
+        assert_eq!(wgs84.a, 6_378_137.0);
+        assert_eq!(wgs84.invf, 298.257_223_563);
     }
 }
---
+++ geokit/tests/llh_to_xyz.rs
@@ -0,0 +1,57 @@
+use geokit::{coord::Coord3D, geodesy::Ellipsoid};
+use regex::Regex;
+use std::default::Default;
+
+#[test]
+fn llh_to_xyz() {
+    let ellipsoid = Ellipsoid::default();
+
+    let re = Regex::new(r"\s+").unwrap();
+    let ll_grid = include_str!("data/ll_grid.txt");
+    let llhs = ll_grid
+        .lines()
+        .filter(|l| !l.is_empty() && !l.starts_with("#"))
+        .map(|l| {
+            re.split(l.trim())
+                .map(|s| s.parse::<f64>().unwrap())
+                .collect::<Vec<f64>>()
+        })
+        .map(|v| (v[0].to_radians(), v[1].to_radians(), 0.0))
+        .collect::<Vec<Coord3D>>();
+
+    let xyz_grid = include_str!("data/xyz_grid.txt");
+    let xyzs = xyz_grid
+        .lines()
+        .filter(|l| !l.is_empty() && !l.starts_with("#"))
+        .map(|l| {
+            re.split(l.trim())
+                .map(|s| s.parse::<f64>().unwrap())
+                .collect::<Vec<f64>>()
+        })
+        .map(|v| (v[0], v[1], v[2]))
+        .collect::<Vec<Coord3D>>();
+
+    for (llh, xyz) in llhs.iter().zip(xyzs.iter()) {
+        let computed_xyz = ellipsoid.llh_to_xyz(llh);
+        assert!(
+            (xyz.0 - computed_xyz.0).abs() < 1e-3
+                && (xyz.1 - computed_xyz.1).abs() < 1e-3
+                && (xyz.2 - computed_xyz.2).abs() < 1e-3,
+            "{:?} -> {:?} != {:?}",
+            llh,
+            computed_xyz,
+            xyz
+        );
+
+        let computed_llh = ellipsoid.xyz_to_llh(xyz);
+        assert!(
+            (llh.0 - computed_llh.0).abs() < 1e-3
+                && (llh.1 - computed_llh.1).abs() < 1e-3
+                && (llh.2 - computed_llh.2).abs() < 1e-3,
+            "{:?} -> {:?} != {:?}",
+            xyz,
+            computed_llh,
+            llh
+        );
+    }
+}
